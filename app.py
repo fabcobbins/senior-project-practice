fe355b5ae0a44f9e0a33713f2b46b7146afd221c
fabcobbins <168049953+fabcobbins@users.noreply.github.com>
2026-09-08 14:35:30 -0400

    Add initial developer profile

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-
+print("Senior Project Developer Profile")
+print()
+print("Name: Michael Cobbins")
+print("Major: Computer Science")
+print("Technology Interest: Blockchain")
+print("Skill Goal: Smart Contract Development")
