04b47ec84fe8219d56dd9e180a2d8cab7cd2c0ea
fabcobbins <168049953+fabcobbins@users.noreply.github.com>
2026-09-08 14:38:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
 
+print("Senior Project Developer Profile")
+print()
+print("Name: Michael Cobbins")
+print("Major: Computer Science")
+print("Technology Interest: Blockchain")
+print("Skill Goal: Smart Contract Development")
